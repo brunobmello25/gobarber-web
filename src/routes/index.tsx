5da1e350f8d899e311ffe6dbda1783f3815f41a7
Bruno Mello <bruno.barros.mello@gmail.com>
2020-05-31 15:48:47 -0300

    BM: add authenticated routes with custom route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { SignIn, SignUp } from 'pages';
+import { Switch } from 'react-router-dom';
+import { SignIn, SignUp, Dashboard } from 'pages';
+import Route from './Route';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
+
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
